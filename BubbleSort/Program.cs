624bc0a3a9323bcaead5cf998f9412987c5c5096
firdaussamad <firdaussamad94@gmail.com>
2016-02-22 09:09:18 +0800

    first commit

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is Bubble sort program.
+            int[] list = new int[] { 20, 1, 3, 2, 10, 7 };
+            int temp;
+            for (int k = 0; k < list.Length; k++)
+            {
+                for (int i = 0; i < list.Length; i++)
+                {
+                    if (i != list.Length - 1 && (list[i] > list[i + 1]))
+                    {
+                        temp = list[i];
+                        list[i] = list[i + 1];
+                        list[i + 1] = temp;
+                    }
+                }
+            }
+            foreach(int a in list)
+            {
+                Console.WriteLine(a);
+            }
+            Console.ReadLine();
+        }
+    }
+}
